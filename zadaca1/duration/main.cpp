e4cde090232e3192388ee0e5c0da0df1c0f0c44a
moucheh <moucheh@proton.me>
2024-03-11 10:52:07 +0100

    Zadaca 1 / Zadatak Duration

---
+++ zadaca1/duration/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "duration.hpp"
+
+int main() {
+	// ovdje možete samostalno testirati neke funkcionalnosti.
+	// Napomena: main.cpp se ne pregleda, sva implementacija
+	// ide u duration .cpp i .hpp fileove.
+	// Duration d1{10, 35, 15};
+	// // Duration d2{10, 40, 15};
+	// std::cout << d1.get_h() << ':' << d1.get_m() << ':' << d1.get_s() << std::endl;
+	// // std::cout << (d1 == d1) << std::endl;
+	// // std::cout << (d1 != d1) << std::endl;
+	// // std::cout << (d1 + Duration{3, 2, 1}) << std::endl;
+	// // Duration d1{10, 35, 15};
+	// Duration d2{10, 40, 15};
+	// std::cout << ((d1 == d1) ? "true" : "false") << std::endl;
+	// std::cout << ((d1 != d1) ? "true" : "false") << std::endl;
+	// std::cout << ((d1 > d2) ? "true" : "false") << std::endl;
+	// std::cout << ((d1 < d2) ? "true" : "false") << std::endl;
+	// std::cout << ((d1 >= d2) ? "true" : "false") << std::endl;
+	// std::cout << ((d1 <= d2) ? "true" : "false") << std::endl;
+	// Duration d1{10, 45, 50};
+	// Duration d2{4, 25, 30};
+	// try {
+	// 	d2 - d1;
+	// } catch (const std::out_of_range& e) {
+	// 	std::cout << e.what() << std::endl;
+	// }
+	std::cout << Duration("10:30:20") << std::endl;
+	return 0;
+}
