7835b7b78531b05867f816733303a36ad03099fe
moucheh <moucheh@proton.me>
2024-03-21 13:29:36 +0100

    added iterator traits

--- vjezba/my_list/List.hpp
+++ vjezba/my_list/List.hpp
@@ -7,6 +7,12 @@
 template<typename T>
 class List {
 public:
+	typedef std::ptrdiff_t difference_type;
+	typedef T value_type;
+	typedef T* pointer;
+	typedef T& reference;
+	typedef std::bidirectional_iterator_tag iterator_category;
+public:
 	struct Node {
 		T data{};
 		Node* next{nullptr};
